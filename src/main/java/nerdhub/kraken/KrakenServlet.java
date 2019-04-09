0acd1e23ed2d377b6229690796b9456cc2b69cc2
abused_master <techperson71@gmail.com>
2019-04-09 12:43:44 -0400

    Moved electron package out to main

--- src/main/java/nerdhub/kraken/KrakenServlet.java
+++ src/main/java/nerdhub/kraken/KrakenServlet.java
@@ -5,7 +5,7 @@ import com.vaadin.server.VaadinServlet;
 
 import javax.servlet.annotation.WebServlet;
 
-@WebServlet(urlPatterns = "/*", name = "KrakenServlet")
+@WebServlet(urlPatterns = "/*", name = "KrakenServlet", asyncSupported = true)
 @VaadinServletConfiguration(ui = KrakenUI.class, productionMode = false)
 public class KrakenServlet extends VaadinServlet {
 }
