f4214a314359a6b62221c6f68d7a3033d64b4f5c
abused_master <techperson71@gmail.com>
2019-04-12 17:33:06 -0400

    Moved to NodeJS

---
+++ app.js
@@ -0,0 +1,32 @@
+const {app, BrowserWindow} = new require('electron');
+
+let window;
+
+function createWindow() {
+    window = new BrowserWindow({
+        title: 'Kraken Launcher',
+        icon: 'view/assets/images/favicon.ico',
+        width: 950,
+        height: 700
+    });
+
+    window.setResizable(false);
+    window.setMenu(null);
+    window.loadFile('view/index.html');
+
+    window.on('closed', () => {
+       window = null;
+    });
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+   if(process.platform !== 'darwin') {
+       app.quit();
+   }
+});
+app.on('activate', () => {
+   if(window === null) {
+       createWindow();
+   }
+});
